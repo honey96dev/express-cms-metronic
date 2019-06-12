bf2872f5e2b3f844067053835eb19ad985e76e0c
Fullservice Fullstacked <honey96dev@gmail.com>
2019-06-11 22:24:47 -0700

    Update dashboard.js

--- routes/admin/dashboard.js
+++ routes/admin/dashboard.js
@@ -6,7 +6,7 @@ import sprintfJs from 'sprintf-js';
 const router = express.Router();
 
 const dashboardProc = (req, res, next) => {
-    console.log(req.session);
+    // console.log(req.session);
     res.render('admin/dashboard', {
         userName: req.session.admin.name,
         title: 'Dashboard',
