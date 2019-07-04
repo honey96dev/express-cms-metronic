9dddd769189b4e43a22034e4c5cfbfcdb4292906
Fullservice Fullstacked <honey96dev@gmail.com>
2019-07-04 14:35:47 -0700

    small change of ui

--- public/javascripts/site/propietarios/propiedades/add.js
+++ public/javascripts/site/propietarios/propiedades/add.js
@@ -5,24 +5,29 @@ function AddProperty() {
 }
 
 AddProperty.prototype.init = function() {
-    this.typeView = $('#property_type').select2({
+    let self = this;
+    self.typeView = $('#type').select2({
         placeholder: "Type",
     });
-    this.roomsView = $('#property_rooms').select2({
+    self.roomsView = $('#rooms').select2({
         placeholder: "Rooms",
     });
-    this.bathView = $('#property_baths').select2({
+    self.bathView = $('#baths').select2({
         placeholder: "Baths",
     });
-    this.m2View = $('#property_m2').select2({
+    self.m2View = $('#m2').select2({
         placeholder: "m2",
     });
-    this.mapView = new GMaps({
-        div: '#property_gmap',
+    self.mapView = new GMaps({
+        div: '#gmap',
         lat: -12.043333,
         lng: -77.028333
     });
-}
+
+    $('#addButton').click(function (e) {
+        console.log(e);
+    });
+};
 
 $(document).ready(function () {
     instance = new AddProperty();
