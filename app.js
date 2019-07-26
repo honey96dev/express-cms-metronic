9708beed4ad8e5f5359c6faae602119d17fe4101
honey96dev <honey96dev@gmail.com>
2019-07-25 20:54:04 -0800

    Update app.js

--- app.js
+++ app.js
@@ -48,15 +48,15 @@ app.use(cookieSessionLib({
     // maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
-// app.use(function (req, res, next) {
-//     if (req.secure || config.server.isDev) {
-//         // request was via https, so do no special handling
-//         next();
-//     } else {
-//         // request was via http, so redirect to https
-//         res.redirect('https://' + req.headers.host + req.url);
-//     }
-// });
+app.use(function (req, res, next) {
+    if (req.secure || config.server.isDev) {
+        // request was via https, so do no special handling
+        next();
+    } else {
+        // request was via http, so redirect to https
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+});
 
 app.use(fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
